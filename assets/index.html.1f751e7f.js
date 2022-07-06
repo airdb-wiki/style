97883167c03c751c412678791604a0832e4c083d
deancn <deancn@users.noreply.github.com>
2022-07-06 06:35:41 +0000

    deploy: 49875a07f1e818883f932e3e4ae4b653d27890f4

---
+++ assets/index.html.1f751e7f.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-bbaa4628","path":"/coding/","title":"Coding Styles","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1657089300000,"contributors":[{"name":"deancn","email":"dean@airdb.com","commits":2}]},"filePathRelative":"coding/README.md"}');export{e as data};
